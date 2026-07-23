48f4a0b45ce98e586d5a430f7a2607e658f4c6a8
tingx222 <2070574107@qq.com>
2026-07-23 20:29:52 +0800

    基于redis的分布式锁-lua

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -2,8 +2,12 @@ package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -18,6 +22,14 @@ public class SimpleRedisLock implements ILock{
 
     public static final String KEY_PREFIX = "lock:";
     public static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    //lua脚本的初始化
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
 
     @Override
@@ -37,6 +49,16 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
+        //调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
+    }
+
+    /*    @Override
+    public void unlock() {
         //获取线程标示
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         //获取锁的标示
@@ -46,5 +68,5 @@ public class SimpleRedisLock implements ILock{
             //释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
---
+++ src/main/resources/unlock.lua
@@ -0,0 +1,6 @@
+--比较线程和锁中的标示是否一致
+if redis.call('get', KEYS[1]) == ARGV[1] then
+    --释放锁
+    return redis.call('del', KEYS[1])
+end
+return 0
